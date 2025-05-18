9b65ebe6faecc2be62f3b7b579399234817fd560
Connor van Spronssen <info@dobefu.com>
2025-05-18 12:34:28 +0200

    feat: Add a log level CLI flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
-	"os"
-
+	"github.com/Dobefu/web2paper/internal/logger"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	logLevel int
 )
 
 var rootCmd = &cobra.Command{
@@ -11,10 +15,16 @@ var rootCmd = &cobra.Command{
 	Short: "Quickly and easily convert an HTML document to PDF",
 }
 
-func Execute() {
-	err := rootCmd.Execute()
+func init() {
+	cobra.OnInitialize(initConfig)
 
-	if err != nil {
-		os.Exit(1)
-	}
+	rootCmd.PersistentFlags().IntVarP(&logLevel, "loglevel", "l", logger.LogLevelInfo, "The log level to use. 0=TRACE, 1=DEBUG, 2=INFO, 3=WARN, 4=ERROR, 5=FATAL")
+}
+
+func initConfig() {
+	viper.Set("log.level", logLevel)
+}
+
+func Execute() {
+	_ = rootCmd.Execute()
 }
