d36590d9abab3c656abc3ba70e018def05b85654
Connor van Spronssen <info@dobefu.com>
2025-05-18 21:15:20 +0200

    test: Add support for extra flags in the test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ set -e
 cd "$(dirname "$0")/.."
 
 # Run the tests.
-go test "./..." -v -coverprofile="coverage.out" -covermode=count -parallel="$(nproc)"
+go test "./..." "$@" -coverprofile="coverage.out" -covermode=count -parallel="$(nproc)"
 
 # Display the coverage statistics and generate an HTML report.
 go tool cover -func coverage.out
