7476757d056ed917501f54fc4f271c64cc22436a
Connor van Spronssen <info@dobefu.com>
2025-05-19 20:49:48 +0200

    test: Add a test to the convert method

--- internal/converter/converter_test.go
+++ internal/converter/converter_test.go
@@ -40,3 +40,36 @@ func TestConverterNew(t *testing.T) {
 		})
 	}
 }
+
+func TestConverterConvert(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		input         string
+		output        string
+		shouldSucceed bool
+	}{
+		"success": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			shouldSucceed: true,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			pdfConverter, err := New(test.input, test.output)
+			assert.NoError(t, err)
+
+			err = pdfConverter.Convert()
+
+			if test.shouldSucceed {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
