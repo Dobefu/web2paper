f48e98aa0af2f5a97ff05a1b33c3f00b1a53abcb
Connor van Spronssen <info@dobefu.com>
2025-05-25 16:26:02 +0200

    test: Add tests to the formatTextObj method

---
+++ internal/converter/format_text_obj_test.go
@@ -0,0 +1,86 @@
+package converter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFormatTextObj(t *testing.T) {
+	tests := map[string]struct {
+		input         string
+		output        string
+		text          string
+		init          func(textOptions *_textOptions)
+		shouldContain string
+	}{
+		"success": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          nil,
+			shouldContain: "",
+		},
+		"custom spacing": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.Spacing = 99 },
+			shouldContain: "99 Tc",
+		},
+		"custom word spacing": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.WordSpacing = 99 },
+			shouldContain: "99 Tw",
+		},
+		"custom scale": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.Scale = 99 },
+			shouldContain: "99 Tz",
+		},
+		"custom leading": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.Leading = 99 },
+			shouldContain: "99 TL",
+		},
+		"custom rendering mode": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.RenderingMode = renderingModeStroke },
+			shouldContain: "1 Tr",
+		},
+		"custom rise": {
+			input:         "testdata/001_empty_page/index.html",
+			output:        "out.pdf",
+			text:          "Test text",
+			init:          func(textOptions *_textOptions) { textOptions.Rise = 99 },
+			shouldContain: "99 Ts",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			c, err := New(test.input, test.output)
+			assert.NoError(t, err)
+
+			textOptions := textOptionsNew()
+
+			if test.init != nil {
+				test.init(&textOptions)
+			}
+
+			textObj := c.formatTextObj(24, 0, 0, test.text, textOptions)
+			assert.Contains(t, string(textObj), "BT")
+			assert.Contains(t, string(textObj), test.text)
+			assert.Contains(t, string(textObj), test.shouldContain)
+			assert.Contains(t, string(textObj), "ET")
+		})
+	}
+}
