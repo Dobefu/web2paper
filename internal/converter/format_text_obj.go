90849224d33ff12387664218a97cb0a4293e14f7
Connor van Spronssen <info@dobefu.com>
2025-05-25 15:48:58 +0200

    feat: Add some more parameters to the formatTextObj method

--- internal/converter/format_text_obj.go
+++ internal/converter/format_text_obj.go
@@ -5,12 +5,23 @@ import (
 	"fmt"
 )
 
-func (c *converter) formatTextObj(fontSize int, x int, y int, text string) (textObj []byte) {
-	buf := bytes.NewBuffer([]byte("BT\n"))
-	fmt.Fprintf(buf, "F1 %d Tf\n", fontSize)
-	fmt.Fprintf(buf, "%d %d Td\n", x, y)
-	fmt.Fprintf(buf, "(%s) Tj\n", text)
-	buf.WriteString("ET")
+func (c *converter) formatTextObj(
+	fontSize int,
+	x int,
+	y int,
+	text string,
+) (textObj []byte) {
+	buf := bytes.NewBuffer([]byte("BT\n"))   // "Begin Text".
+	fmt.Fprintf(buf, "F1 %d Tf\n", fontSize) // Font and font size.
+	fmt.Fprintf(buf, "%d %d Td\n", x, y)     // Coordinates.
+	fmt.Fprintf(buf, "0 Tc\n")               // Text spacing.
+	fmt.Fprintf(buf, "0 Tw\n")               // Text word spacing.
+	fmt.Fprintf(buf, "100 Tz\n")             // Text scale % (horizontal).
+	fmt.Fprintf(buf, "0 TL\n")               // Text leading.
+	fmt.Fprintf(buf, "0 Tr\n")               // Text rendering mode.
+	fmt.Fprintf(buf, "0 Ts\n")               // Text rise.
+	fmt.Fprintf(buf, "(%s) Tj\n", text)      // Text content.
+	buf.WriteString("ET")                    // "End Text".
 
 	return buf.Bytes()
 }
