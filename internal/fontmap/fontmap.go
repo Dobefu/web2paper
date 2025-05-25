057a49d616efe94ef29f5cf56efe79237ba894d4
Connor van Spronssen <info@dobefu.com>
2025-05-25 18:41:04 +0200

    feat: Add a method to the fontmap struct to get the width of a text string

--- internal/fontmap/fontmap.go
+++ internal/fontmap/fontmap.go
@@ -4,3 +4,11 @@ type fontmap struct {
 	CharHeight int
 	CharWidths map[rune]int
 }
+
+func (f *fontmap) GetTextWidth(text string, fontSize int) (width int) {
+	for _, glyph := range text {
+		width += f.CharWidths[glyph]
+	}
+
+	return width * fontSize / 1000
+}
