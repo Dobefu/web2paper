6c72a44b6b3b8214922be5c2878a9a89a3ce6a50
Connor van Spronssen <info@dobefu.com>
2025-05-28 09:00:05 +0200

    refactor: Move the GetTextWidth method to a separate file

--- internal/fontmap/fontmap.go
+++ internal/fontmap/fontmap.go
@@ -7,17 +7,3 @@ type fontmap struct {
 	XHeight    int
 	CharWidths map[rune]int
 }
-
-func (f *fontmap) GetTextWidth(text string, fontSize int) (width float32) {
-	for _, glyph := range text {
-		glyphWidth, ok := f.CharWidths[glyph]
-
-		if !ok {
-			glyphWidth = f.CharWidths[' ']
-		}
-
-		width += float32(glyphWidth)
-	}
-
-	return width * float32(fontSize) / 1000
-}
---
+++ internal/fontmap/get_text_width.go
@@ -0,0 +1,15 @@
+package fontmap
+
+func (f *fontmap) GetTextWidth(text string, fontSize int) (width float32) {
+	for _, glyph := range text {
+		glyphWidth, ok := f.CharWidths[glyph]
+
+		if !ok {
+			glyphWidth = f.CharWidths[' ']
+		}
+
+		width += float32(glyphWidth)
+	}
+
+	return width * float32(fontSize) / 1000
+}
