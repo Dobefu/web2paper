66ff4e3c20afb4951a2c946aee409bc9fea15cdb
Connor van Spronssen <info@dobefu.com>
2025-05-28 15:19:38 +0200

    test: Add a benchmark to the collectMetadata method

---
+++ internal/html_parser/collect_metadata_test.go
@@ -0,0 +1,18 @@
+package html_parser
+
+import (
+	"os"
+	"testing"
+)
+
+func BenchmarkCollectMetadata(b *testing.B) {
+	data, _ := os.ReadFile("../converter/testdata/002_text_page/index.html")
+
+	p := HtmlParser{}
+	_ = p.ParseHtml(data)
+	b.ResetTimer()
+
+	for b.Loop() {
+		p.collectMetadata()
+	}
+}
